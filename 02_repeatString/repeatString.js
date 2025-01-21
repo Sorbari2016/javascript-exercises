583e2709fc41be405c61e62e05b1857bfe02ae49
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2025-01-21 20:16:12 +0100

    Completed the repeatstring testing tasks

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,31 @@
-const repeatString = function() {
+function repeatString(str, num, useRandom = false) {
+  // If useRandom is true, generate a random number
+  if (useRandom) {
+    num = Math.floor(Math.random() * 1000);
+  }
 
-};
+  if (num < 0) {
+    return "ERROR"; // Throw an error for negative numbers
+  }
+
+  if (num === 0) {
+    return ""; // Return an empty string for 0 repetitions
+  }
+
+  // Explicitly check if the string is exactly a blank space
+  if (str === " ") {
+    return " ".repeat(num); // Repeat the blank space for the specified times
+  }
+
+  let result = "";
+  for (let i = 0; i < num; i++) {
+    result += str;
+  }
+  return result;
+}
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
