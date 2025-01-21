be67a71af5e87aa73931d026ec33c5114c3c43f2
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2025-01-21 21:05:03 +0100

    completed the reversestring test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
-};
-
+function reverseString(str) {
+    // Split the string using a regular expression to capture spaces
+    return str
+      .split(/(\s+)/) // Split by spaces while keeping spaces as part of the array
+      .reverse() // Reverse the array of words and spaces
+      .map(word => word.split("").reverse().join("")) // Reverse each word or space group
+      .join(""); // Join everything back without adding extra spaces
+  }
+  
+  
+  
 // Do not edit below this line
 module.exports = reverseString;
