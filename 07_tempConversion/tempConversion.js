97101e18f76f522b1cc597acf3ee50ff4ac4d15b
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2025-01-22 22:49:02 +0100

    Completed the convesion tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
-};
+function convertToCelsius(fahrenheit) {
+  // Convert Fahrenheit to Celsius
+  let celsius = (fahrenheit - 32) * (5 / 9);
+  // Round to one decimal place
+  return Math.round(celsius * 10) / 10;
+}
 
-const convertToFahrenheit = function() {
-};
+function convertToFahrenheit(celsius) {
+  // Convert Celsius to Fahrenheit
+  let fahrenheit = (celsius * (9 / 5)) + 32;
+  // Round to one decimal place
+  return Math.round(fahrenheit * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
