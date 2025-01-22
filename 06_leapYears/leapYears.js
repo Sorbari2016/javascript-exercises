e75804e40d139f262e8154426199c6d2baec7455
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2025-01-22 22:11:02 +0100

    Completed the leapyear tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
-
-};
+function leapYears(year) {
+    // Check if the year is divisible by 4
+    if (year % 4 === 0) {
+      // If the year is also divisible by 100, check if it's divisible by 400
+      if (year % 100 === 0) {
+        return year % 400 === 0;
+      }
+      // Otherwise, it's a leap year
+      return true;
+    }
+    // If not divisible by 4, it's not a leap year
+    return false;
+  }
 
 // Do not edit below this line
 module.exports = leapYears;
