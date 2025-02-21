9e7c663ef5b549d00e7c23b8f9f25161d304e681
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2025-02-21 14:37:35 +0100

    Completed the palindromes tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
+function palindromes(str) {
+    // Remove non-alphanumeric characters and convert to lowercase
+    let cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    // Reverse the string and compare
+    return cleanStr === cleanStr.split('').reverse().join('');
+}
 
-};
+
 
 // Do not edit below this line
 module.exports = palindromes;
