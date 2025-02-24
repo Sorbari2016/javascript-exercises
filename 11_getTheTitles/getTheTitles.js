082ea642b39ff60bb0d09eb7fde51246c38bd744
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2025-02-24 20:53:12 +0100

    Completed the get the titles tests

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,14 @@
-const getTheTitles = function() {
-
-};
+function getTheTitles(books) {
+    return books.map(function(book) {
+      return book.title;
+    });
+  }
+  
+  // Example usage:
+  const books = [
+    { title: 'Book', author: 'Name' },
+    { title: 'Book2', author: 'Name2' }
+  ];
 
 // Do not edit below this line
 module.exports = getTheTitles;
